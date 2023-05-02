39d9f6e73b9f50c111374c6e57ddd84efcef38af
Erin Crews <61025723+eecrews@users.noreply.github.com>
2023-05-01 23:22:41 -0500

    still werkin

--- ART-RAT/Assets/Scripts/TransformSpaces.cs
+++ ART-RAT/Assets/Scripts/TransformSpaces.cs
@@ -4,16 +4,13 @@ using UnityEngine;
 
 public class TransformSpaces : MonoBehaviour
 {
-
-
-
     public bool position_on = false;
     public bool rotation_on = false;
 
-    public float magnitude_scale = 100f;
-
+    public float magnitude_scale;
     public GameObject source_transform;
     public GameObject target_transform;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -22,12 +19,12 @@ public class TransformSpaces : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
-
         //position
         if (position_on) target_transform.transform.localPosition = source_transform.transform.position * magnitude_scale;
 
         //rotation
         if (rotation_on) target_transform.transform.localEulerAngles = source_transform.transform.eulerAngles * magnitude_scale;
 
+        //.log(target_transform.transform.localPosition);
     }
 }
